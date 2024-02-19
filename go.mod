531a5363df5b2f1ffc1e6cb4e3aba62ffeb91b3a
doot <126783585+dootss@users.noreply.github.com>
2024-02-19 23:47:47 +0000

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/Paguiar735/whatsmeow-quickstart
+
+go 1.21
+
+toolchain go1.22.0
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/mdp/qrterminal/v3 v3.2.0
+	go.mau.fi/whatsmeow v0.0.0-20240214134033-43138278d3ed
+	google.golang.org/protobuf v1.32.0
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.32.0 // indirect
+	go.mau.fi/libsignal v0.1.0 // indirect
+	go.mau.fi/util v0.4.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "os"
+    "os/signal"
+    "syscall"
+
+    _ "github.com/mattn/go-sqlite3"
+    "github.com/mdp/qrterminal/v3"
+
+    "go.mau.fi/whatsmeow"
+    waProto "go.mau.fi/whatsmeow/binary/proto"
+    "go.mau.fi/whatsmeow/store/sqlstore"
+    "go.mau.fi/whatsmeow/types"
+    "go.mau.fi/whatsmeow/types/events"
+    waLog "go.mau.fi/whatsmeow/util/log"
+    "google.golang.org/protobuf/proto"
+)
+
+func GetEventHandler(client *whatsmeow.Client) func(interface{}) {
+    return func(evt interface{}) {
+        switch v := evt.(type) {
+        case *events.Message:
+            // Ensure the message is a text message
+            if v.Info.Type != "text" {
+                return
+            }
+
+            // Retrieve the client's own phone number from the client's JID
+            ownNumber := client.Store.ID.User
+
+            // Parse the message text
+            var messageBody string
+            if v.Message.GetExtendedTextMessage() != nil {
+                messageBody = v.Message.GetExtendedTextMessage().GetText()
+            } else {
+                messageBody = v.Message.GetConversation()
+            }
+
+            // Check if the message starts with "!status"
+            if len(messageBody) > 7 && messageBody[:7] == "!status" {
+                var reaction *waProto.Message
+                if v.Info.Sender.User == ownNumber {
+                    // Extract the status message
+                    statusText := messageBody[8:]
+
+                    // Construct the status update message
+                    statusMessage := &waProto.Message{
+                        ExtendedTextMessage: &waProto.ExtendedTextMessage{
+                            Text:           proto.String(statusText),
+                            BackgroundArgb: proto.Uint32(0xFFFFFFFF), // Example ARGB color (black)
+                            TextArgb:       proto.Uint32(0xFF000000), // Example ARGB color (white)
+                            Font:           waProto.ExtendedTextMessage_SYSTEM.Enum(), // Example font
+                        },
+                    }
+
+                    // Use types.StatusBroadcastJID for posting status updates
+                    _, err := client.SendMessage(context.Background(), types.StatusBroadcastJID, statusMessage)
+                    if err != nil {
+                        fmt.Printf("Failed to post status update: %v\n", err)
+                        reaction = client.BuildReaction(v.Info.Chat, v.Info.Sender, v.Info.ID, "❌")
+                    } else {
+                        fmt.Println("Status update posted successfully.")
+                        reaction = client.BuildReaction(v.Info.Chat, v.Info.Sender, v.Info.ID, "✅")
+                    }
+                } else {
+                    // If the sender is not the authenticated user, react with a cross
+                    reaction = client.BuildReaction(v.Info.Chat, v.Info.Sender, v.Info.ID, "❌")
+                }
+
+                // Send the reaction
+                if reaction != nil {
+                    _, err := client.SendMessage(context.Background(), v.Info.Chat, reaction)
+                    if err != nil {
+                        fmt.Printf("Failed to send reaction: %v\n", err)
+                    }
+                }
+            }
+        }
+    }
+}
+
+func main() {
+    dbLog := waLog.Stdout("Database", "DEBUG", true)
+    container, err := sqlstore.New("sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
+    if err != nil {
+        panic(err)
+    }
+
+    deviceStore, err := container.GetFirstDevice()
+    if err != nil {
+        panic(err)
+    }
+
+    clientLog := waLog.Stdout("Client", "INFO", true)
+    client := whatsmeow.NewClient(deviceStore, clientLog)
+    client.AddEventHandler(GetEventHandler(client))
+
+    client.DontSendSelfBroadcast = false
+
+    if client.Store.ID == nil {
+        qrChan, _ := client.GetQRChannel(context.Background())
+        err = client.Connect()
+        if err != nil {
+            panic(err)
+        }
+        for evt := range qrChan {
+            if evt.Event == "code" {
+                qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+            } else {
+                fmt.Println("Login event:", evt.Event)
+            }
+        }
+    } else {
+        err = client.Connect()
+        if err != nil {
+            panic(err)
+        }
+        // Update presence status
+        err = client.SendPresence(types.PresenceAvailable)
+        if err != nil {
+            fmt.Printf("Failed to update presence: %v\n", err)
+        }
+    
+        // Check status privacy settings
+        statusPrivacy, err := client.GetStatusPrivacy()
+        if err != nil {
+            fmt.Printf("Failed to get status privacy settings: %v\n", err)
+        } else {
+            fmt.Println("Status Privacy Settings:", statusPrivacy)
+        }
+    }
+
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    <-c
+
+    client.Disconnect()
+}
